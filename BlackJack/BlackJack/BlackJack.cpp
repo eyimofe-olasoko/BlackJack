ea6e576adad564a837b9ff4ee599d6395dd99ab3
Mugen <52742495+eyimofe-olasoko@users.noreply.github.com>
2024-02-05 15:44:07 +0000

    Creating the class

---
+++ BlackJack/BlackJack/BlackJack.cpp
@@ -0,0 +1,47 @@
+// BlackJack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <string>
+#include <iostream>
+#include "Cards.h"
+using namespace std;
+
+int main()
+{
+	stack<string> List;
+
+	List.push("");
+
+	Cards Cards0 = Cards("Diamond", 10);
+	Cards Cards1 = Cards("Clubs", 4);
+
+	
+	Cards Deck[52];
+
+	stack<int> DiamondSuit
+	{
+
+
+	};
+
+	for (size_t item = 0; item < 13; item++)
+	{
+		
+	}
+
+
+
+	/*
+	int ages[] = {1, 2, 5, 7, 3};
+	int sum = 0;
+
+	for (size_t item = 0; item < 5; item++)
+	{
+		cout << ages[item] << endl;
+
+		sum += ages[item];
+	}
+
+	cout << "The sum of the your numbers: " << sum << endl;
+	*/
+}
+
